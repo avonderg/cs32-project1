24f14a2d3eabde0560ee750b812bfc51269d29a3
Ben Piekarz <ben_piekarz@brown.edu>
2022-02-13 11:25:40 -0500

    added KD stencil

---
+++ src/main/java/edu/brown/cs/student/main/KDNode.java
@@ -0,0 +1,13 @@
+package edu.brown.cs.student.main;
+
+public interface KDNode {
+
+  public double getCoord(int dim);
+
+
+}
+
+
+//havea  kd tree class, and generic with generic
+//KDTree<T extends KDTreeNode>
+//T might just be like students, studebt doesn't need t
---
+++ src/main/java/edu/brown/cs/student/main/KDTree.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.student.main;
+
+import java.util.List;
+
+public class KDTree<T extends KDNode> {
+
+  public KDTree(List studentList) {
+
+  }
+
+
+
+
+}
