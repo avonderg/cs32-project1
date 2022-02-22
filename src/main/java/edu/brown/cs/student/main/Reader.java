7aca6a879da3bbce83f254644c6c149cdb1f9104
Kleo <32969977+kleookku@users.noreply.github.com>
2022-02-22 16:42:58 -0500

    Update Reader.java

--- src/main/java/edu/brown/cs/student/main/Reader.java
+++ src/main/java/edu/brown/cs/student/main/Reader.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
+/**
+ * The CSV Reader class. This where file names are passed in and their information is stored.
+ */
 public class Reader {
 
-  private List<String> tokens;
-
   private Data data;
-
 
   /**
    * Class constructor.
@@ -22,28 +21,39 @@ public class Reader {
   }
 
   /**
-   * Reads data from CSV file with given file name and stores it in data hashmap.
-   * @param fileName
+   * Reads data from CSV file with given file name and stores it in Data object.
+   * @param fileName -- name of the csv file to be read
    */
   public void loadData(String fileName) {
+    // store the filePath object from given file name
     Path filePath  = Paths.get(fileName);
-    try (BufferedReader reader = Files.newBufferedReader(filePath)) { // read file
-      if (this.data.size() == 0) {
-        data.clear(); // clears out old hashmap
-      }
-      String line = reader.readLine(); // read header first, don't add to hashmap
+
+    // initialize bufferedReader to read the file
+    try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+      // clear out old data
+      data.clear();
+
+      // read header first, so don't store header information as data
+      String line = reader.readLine();
+
+      // iterate through lines of csv until reached the end (null)
       while ((line = reader.readLine()) != null) {
-        //Student student = new Student(line);
+
+        // calls data object's insert method to store information in current line of the csv file
         data.insert(line);
       }
-//      System.out.println("Read " + data.size() + " students from " + fileName);
+
+      // prints output describing how much data was read from given file
+      // System.out.println("Read " + data.size() + " lines from " + fileName);
     } catch (IOException ie) {
+
+      // if file cannot be read, print out error statement
       System.out.println("ERROR: File not found.");
     }
   }
 
   /**
-   * Returns hashmap storing the data.
+   * Returns object storing the data.
    * @return
    */
   public Data getData() {
