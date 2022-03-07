5b10dd01954f5a8b80b5f7982ceba3a0dcd5d698
avonderg <77714730+avonderg@users.noreply.github.com>
2022-03-07 14:50:48 -0500

    fixed glitch

--- src/main/java/edu/brown/cs/student/main/repl/ApiDBRecCommand.java
+++ src/main/java/edu/brown/cs/student/main/repl/ApiDBRecCommand.java
@@ -124,6 +124,7 @@ public class ApiDBRecCommand implements Command {
             List<StudentFromDB> dbStudents =
                 this.idToDBstudentMap.values().stream().collect(Collectors.toList());
 
+            System.out.println(dbStudents.get(5));
             for (int i = 0; i < 60; i++) {
                 StudentFromDB currDB = dbStudents.get(i);
                 APIInfoStudents currInfo = infoStudents.get(i);
