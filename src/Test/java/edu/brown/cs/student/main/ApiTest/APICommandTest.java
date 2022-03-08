3d2f43ee34c3a811bd1173fad0e32d9f185cad5e
avonderg <77714730+avonderg@users.noreply.github.com>
2022-03-07 21:24:18 -0500

    alex: some more unit tests

--- src/Test/java/edu/brown/cs/student/main/ApiTest/APICommandTest.java
+++ src/Test/java/edu/brown/cs/student/main/ApiTest/APICommandTest.java
@@ -4,8 +4,10 @@ import edu.brown.cs.student.main.api.client.ClientRequestGenerator;
 import edu.brown.cs.student.main.api.core.APICommand;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,28 +37,30 @@ public class APICommandTest {
         assertEquals(null, command.checkCommand(tokens4));
     }
 
-//    @Test
-//    public void testGetSecuredGetRequest() {
-//        ClientRequestGenerator client = new ClientRequestGenerator();
-//        HttpRequest activeInfo = ClientRequestGenerator.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active");
-//        HttpRequest activeMatch = ClientRequestGenerator.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active");
-//        HttpClient.Builder clientBuilder = HttpClient.newBuilder()
-//                .version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(60));
-//        assertEquals(activeInfo, client.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active"));
-//        assertEquals(activeMatch, client.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active"));
-//    }
-//
-//    @Test
-//    public void testMakeRequest() {
-//        ApiClient client = new ApiClient();
-//
-//        HttpRequest activeInfo = ClientRequestGenerator.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active");
-//        HttpRequest activeMatch = ClientRequestGenerator.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active");
-//        HttpClient.Builder clientBuilder = HttpClient.newBuilder()
-//                .version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(60));
-//        assertEquals(1, client.makeRequest(activeInfo, 0));
-//        assertEquals(1, client.makeRequest(activeMatch, 0));
-//    }
+    @Test
+    public void testGetSecuredGetRequest() {
+        ClientRequestGenerator client = new ClientRequestGenerator();
+        HttpRequest activeInfo = ClientRequestGenerator.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active");
+        HttpRequest activeMatch = ClientRequestGenerator.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active");
+        HttpClient.Builder clientBuilder = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(60));
+        assertEquals(activeInfo, client.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active"));
+        assertEquals(activeMatch, client.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active"));
+    }
+
+    @Test
+    public void testMakeRequest() throws IOException, InterruptedException {
+        ApiClient client = new ApiClient(null);
+
+        HttpRequest activeInfo = ClientRequestGenerator.getSecuredGetRequest("https://studentinfoapi.herokuapp.com/get-active");
+        HttpRequest activeMatch = ClientRequestGenerator.getSecuredGetRequest("https://studentmatchapi.herokuapp.com/get-active");
+        HttpClient clientB = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2).connectTimeout(Duration.ofSeconds(60)).build();
+        HttpResponse<String> apiResponseInfo = clientB.send(activeInfo, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> apiResponseMatch = clientB.send(activeMatch, HttpResponse.BodyHandlers.ofString());
+        assertEquals(apiResponseInfo.body(), client.makeRequest(activeInfo, 0).body());
+        assertEquals(apiResponseMatch.body(), client.makeRequest(activeMatch, 0).body());
+    }
 
 
 
